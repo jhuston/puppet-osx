4ad18a5ebe2b5f009ffc180d2c4f508833444f9f
Jeff Huston <huston.jeff@gmail.com>
2013-12-24 12:33:53 -0700

    boxen user to sfx

--- manifests/global/disable_sound_effects.pp
+++ manifests/global/disable_sound_effects.pp
@@ -4,6 +4,7 @@ class osx::global::disable_sound_effects {
 		ensure => present,
 		domain => 'com.apple.systemsound',
 		key => 'com.apple.sound.uiaudio.enabled',
-		value => '-int 0'
+		value => '-int 0',
+		user => $::boxen_user;
 	}
 }
