15644fc043a8fe0fba19be49baefffba12b92bec
Jeff Huston <huston.jeff@gmail.com>
2013-12-24 12:28:52 -0700

    adding disable sound effects

---
+++ manifests/global/disable_sound_effects.pp
@@ -0,0 +1,9 @@
+# Public: Disables UI feedback sound effects
+class osx::global::disable_sound_effects {
+	boxen::osx_defaults { 'Disable sound effects':
+		ensure => present,
+		domain => 'com.apple.systemsound',
+		key => 'com.apple.sound.uiaudio.enabled',
+		value => '-int 0'
+	}
+}
