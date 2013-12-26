5133d19ad54463fd14f3862575332a223fb8d46a
Jeff Huston <huston.jeff@gmail.com>
2013-12-26 14:59:46 -0700

    adding sound/

--- manifests/sound/ui.pp
+++ manifests/sound/ui.pp
@@ -1,12 +1,29 @@
-# Public: Disables UI feedback sound effects
-class osx::sound::ui {
+class osx::sound::ui(
+  $level  = undef,
+  $ensure = 'present') {
 
-	boxen::osx_defaults { 'Disable sound effects':
-		user => $::boxen_user,
-		domain => 'com.apple.systemsound',
-		key => 'com.apple.sound.uiaudio.enabled',
-		type => 'int',
-		value => 0;
-		
-	}
+  validate_re($ensure, '^(present|absent)$', "osx::sound::ui([ensure] must be present or absent, is ${ensure}")
+
+  $enabled_int = $ensure ? {
+    present => 1,
+    default => 0
+  }
+
+  boxen::osx_defaults { 'Toggle UI Sound Effects':
+    user   => $::boxen_user,
+    key    => 'com.apple.sound.uiaudio.enabled',
+    domain => 'com.apple.systemsound',
+    type   => 'int',
+    value  => $enabled_int,
+  }
+
+  if $level != undef {
+    boxen::osx_defaults { 'Toggle UI Sound Effects Volume':
+      user   => $::boxen_user,
+      key    => 'com.apple.sound.beep.volume',
+      domain => 'com.apple.systemsound',
+      type   => 'float',
+      value  => $level,
+    }
+  }
 }
